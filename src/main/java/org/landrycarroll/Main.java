90daab564d6d188b02159b733278ef37c3769199
Landry Carroll <carroll.landry@gmail.com>
2026-01-13 21:28:57 -0500

    feat(fibonacci): implement static recursive method

--- src/main/java/org/landrycarroll/Main.java
+++ src/main/java/org/landrycarroll/Main.java
@@ -2,6 +2,24 @@ package org.landrycarroll;
 
 public class Main {
     static void main() {
+        int index = 10;
+        int output = getFibonacciNumber(index);
+        System.out.println("The Fibonacci number with an index of " + index + " is " + output);
+    }
 
+    static public int getFibonacciNumber(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("index must be < 0");
+        }
+
+        if (index == 0) {
+            return 0;
+        }
+
+        if (index == 1) {
+            return 1;
+        }
+
+        return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
